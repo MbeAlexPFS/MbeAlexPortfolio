f3274c2c4c369bcbd933c021132586188e309074
Mbe Alexandre <alexmbe8921@gmail.com>
2026-06-23 18:27:08 +0000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,8 @@ sed -i "s/<VirtualHost \*:8000>/<VirtualHost *:$PORT>/" /etc/apache2/sites-avail
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:$PORT>/" /etc/apache2/sites-available/000-default.conf
 
 php artisan storage:link --no-interaction 2>/dev/null || true
-php artisan migrate --force --no-interaction
+
+php artisan migrate --force --no-interaction 2>/dev/null || true
 
 php-fpm -D
 
